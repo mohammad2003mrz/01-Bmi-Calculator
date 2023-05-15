1bbe91e57e31e8d3072c6d285f7f140034c31fe8
mohammad2003mrz <gorbanben10@gmail.com>
2023-05-15 17:00:15 +0430

    create input mass and message

--- Bmi.java
+++ Bmi.java
@@ -2,5 +2,7 @@ import java.util.Scanner;
 public class Bmi{
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter Your Mass In Kilograms");
+        double inputMass = input.nextDouble();
     }
 }
